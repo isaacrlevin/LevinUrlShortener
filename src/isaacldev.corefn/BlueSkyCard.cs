c5fcfa8228fbd58ba2b96a10e04f713fff025df1
Isaac Levin <isaac.r.levin@gmail.com>
2024-11-21 14:40:51 -0800

    Add Bluesky support

---
+++ src/isaacldev.corefn/BlueSkyCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isaacldev.corefn
+{
+    public class BlueSkyCard
+    {
+        public string error { get; set; }
+        public string likely_type { get; set; }
+        public string url { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string image { get; set; }
+    }
+}
--- src/isaacldev.corefn/FunctionHost.cs
+++ src/isaacldev.corefn/FunctionHost.cs
@@ -1,20 +1,27 @@
 using Azure.Data.Tables;
+using FishyFlip;
+using FishyFlip.Models;
 using isaacldev.domain;
 using Mastonet;
+using Mastonet.Entities;
 using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using Tweetinvi;
@@ -532,35 +539,146 @@ namespace isaacldev.corefn
 
                 if (linkInfo != null && !linkInfo.Posted)
                 {
-                    var client = new TwitterClient(
-                        TwitterConsumerKey,
-                        TwitterConsumerSecret,
-                        TwitterAccessToken,
-                        TwitterAccessSecret
-                        );
+                    await Tweet(linkInfo);
 
-                    var poster = new TweetsV2Poster(client);
-
-                    ITwitterResult tweetResult = await poster.PostTweet(
-                        new TweetV2PostRequest
-                        {
-                            Text = $"{linkInfo.Title} \n {linkInfo.Message} \n\n {ShortenerBase}{linkInfo.RowKey}"
-                        }
-                    );
-
-                    if (tweetResult.Response.IsSuccessStatusCode == false)
-                    {
-                        throw new Exception(
-                            "Error when posting tweet: " + Environment.NewLine + tweetResult.Content
-                        );
-                    }
-
-
-                    //var massClient = new Mastonet.MastodonClient("fosstodon.org", MastodonAccessToken);
-                    //await massClient.PublishStatus($"{linkInfo.Title} \n {linkInfo.Message.Replace("@", "#")} \n\n {ShortenerBase}{linkInfo.RowKey}", Visibility.Public);
+                    await PostToBlueSky(linkInfo);
                 }
             }
             return new OkObjectResult("");
         }
+
+        private async Task Tweet(ShortUrl linkInfo)
+        {
+
+            if (!Debugger.IsAttached)
+            {
+                var client = new TwitterClient(
+                    TwitterConsumerKey,
+                    TwitterConsumerSecret,
+                    TwitterAccessToken,
+                    TwitterAccessSecret
+                    );
+
+                var poster = new TweetsV2Poster(client);
+
+                ITwitterResult tweetResult = await poster.PostTweet(
+                    new TweetV2PostRequest
+                    {
+                        Text = $"{linkInfo.Title} \n {linkInfo.Message} \n\n {ShortenerBase}{linkInfo.RowKey}"
+                    }
+                );
+
+                if (tweetResult.Response.IsSuccessStatusCode == false)
+                {
+                    throw new Exception(
+                        "Error when posting tweet: " + Environment.NewLine + tweetResult.Content
+                    );
+                }
+            }
+        }
+
+        private async Task PublishToMastodon(ShortUrl linkInfo)
+        {
+            var client = new MastodonClient("fosstodon.org", this.MastodonAccessToken);
+
+            if (!Debugger.IsAttached)
+            {
+                var status = await client.PublishStatus($"{linkInfo.Title} \n {linkInfo.Message} \n\n {ShortenerBase}{linkInfo.RowKey}", new Visibility?((Visibility)0), (string)null, (IEnumerable<string>)null, false, (string)null, new DateTime?(), (string)null, (PollParameters)null);
+            }
+        }
+
+        private async Task PostToBlueSky(ShortUrl linkInfo)
+        {
+            var atProtocol = new ATProtocolBuilder()
+                        .WithLogger(new DebugLoggerProvider().CreateLogger("FishyFlip"))
+            .Build();
+
+            var session = await atProtocol.AuthenticateWithPasswordAsync("isaaclevin.com", "is04aac!");
+
+            if (session is null)
+            {
+                Console.WriteLine("Failed to authenticate.");
+                return;
+            }
+
+            string postTemplate = $"{linkInfo.Title} \n {linkInfo.Message} \n\n {ShortenerBase}{linkInfo.RowKey}";
+
+
+            HttpClient client = new HttpClient();
+            var card = await client.GetFromJsonAsync<BlueSkyCard>(linkInfo.Url);
+
+            Facet? facet = null;
+            FishyFlip.Models.Image? image = null;
+
+            if (card != null)
+            {
+                var uri = new Uri(card.image);
+
+                var uriWithoutQuery = uri.GetLeftPart(UriPartial.Path);
+                var fileExtension = Path.GetExtension(uriWithoutQuery);
+
+                var fileName = card.title;
+
+                var imageBytes = await client.GetByteArrayAsync(uri);
+                await File.WriteAllBytesAsync(Path.Combine(Path.GetTempPath(), $"{fileName}.jpg"), imageBytes);
+
+                var stream = File.OpenRead(Path.Combine(Path.GetTempPath(), $"{fileName}.jpg"));
+                var content = new StreamContent(stream);
+                content.Headers.ContentLength = stream.Length;
+
+                content.Headers.ContentType = new MediaTypeHeaderValue("image/jpg");
+                var blobResult = await atProtocol.Repo.UploadBlobAsync(content);
+
+                await blobResult.SwitchAsync(
+                    async success =>
+                    {
+                        image = success.Blob.ToImage();
+
+                        int promptStart = postTemplate.IndexOf(linkInfo.Url, StringComparison.InvariantCulture);
+                        int promptEnd = promptStart + Encoding.Default.GetBytes(linkInfo.Url).Length;
+                        var index = new FacetIndex(promptStart, promptEnd);
+                        var link = FacetFeature.CreateLink(card.url);
+                        facet = new Facet(index, link);
+                    },
+                    async error =>
+                    {
+                        Console.WriteLine($"Error: {error.StatusCode} {error.Detail}");
+                    }
+                    );
+            }
+
+            if (!Debugger.IsAttached)
+            {
+                if (facet != null && image != null)
+                {
+                    var postResult = await atProtocol.Repo.CreatePostAsync(postTemplate, new[] { facet }, new ImagesEmbed(image, $"Link to {linkInfo.Url}"));
+
+                    postResult.Switch(
+                        success =>
+                        {
+                            Console.WriteLine($"Post: {success.Uri} {success.Cid}");
+                        },
+                        error =>
+                        {
+                            Console.WriteLine($"Error: {error.StatusCode} {error.Detail}");
+                        });
+                }
+                else
+                {
+                    var postResult = await atProtocol.Repo.CreatePostAsync(postTemplate);
+                    postResult.Switch(
+                        success =>
+                        {
+                            Console.WriteLine($"Post: {success.Uri} {success.Cid}");
+                        },
+                        error =>
+                        {
+                            Console.WriteLine($"Error: {error.StatusCode} {error.Detail}");
+                        }
+                        );
+                }
+            }
+        }
+
     }
 }
